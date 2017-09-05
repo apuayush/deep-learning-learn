353e18570dd8c256704a98530268144791574c3e
apuayush <apuayush@gmail.com>
2017-09-05 09:19:37 -0400

    feature scaling

--- Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
+++ Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
@@ -1,8 +1,11 @@
+#This is for including data and handling missing data
+
 # libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.cross_validation import train_test_split
 
 np.set_printoptions(threshold=np.nan)
 
@@ -17,9 +20,17 @@ imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data 
-label_x = LabelEncoder()
-label_y = LabelEncoder()
+label_x = LabelEncoder() # 0 for france 1 for germany and so on
+label_y = LabelEncoder() # 1 for yes and 0 for no
 X[:, 0] = label_x.fit_transform(X[:, 0])
 Y = label_y.fit_transform(Y)
-onehot = OneHotEncoder(categorical_features = [0])
-X = onehot.fit_transform(X).toarray()
+onehot = OneHotEncoder(categorical_features = [0]) #encoding each data with dummy variables 
+X = onehot.fit_transform(X).toarray() 
+
+# dividing data into test and training
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2,random_state = 0)
+
+# feature scaling
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
